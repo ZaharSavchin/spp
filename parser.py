99c9175a9ea501baf7ca7d011fe619d7090837b8
zahar <zman-96@mail.ru>
2023-11-24 13:45:49 +0300

    captcha

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from openpyxl import load_workbook
-mail = 'pasechnik.pchelovod@mail.ru'
+from pass_captcha import tru
 
 
 workbook = load_workbook(filename='wb.xlsx')
@@ -17,29 +17,77 @@ for cell in sheet['A']:
 options_chrome = webdriver.ChromeOptions()
 options_chrome.add_extension('guru.crx')
 
+price_with_spp = []
 price = []
+sold = []
+fidbacks = []
+
+
+def open_widget(browser):
+    browser.find_element(By.CLASS_NAME, "WidgetButton_widgetButtonOpenIcon__uuM6O").click()
 
 
 with webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()), options=options_chrome) as browser:
-    url = 'https://www.wildberries.ru/'
+    url = f'https://www.wildberries.ru/catalog/{arts[3]}/detail.aspx'
     browser.get(url)
-    time.sleep(60)
+    time.sleep(30)
+
+    browser.switch_to.window(browser.window_handles[1])
+
+    tru(browser)
+
+    browser.switch_to.window(browser.window_handles[0])
+
+    browser.get(url)
+    open_widget(browser)
     for art in arts:
-        try:
             browser.get(f'https://www.wildberries.ru/catalog/{art}/detail.aspx')
             time.sleep(5)
-            price_no_spp = browser.find_element(By.CLASS_NAME, "SppWidget_sppWidget__boldPrice__0J7RF").text
-            price_no_spp = ''.join(price_no_spp.split(' ')[:-1])
-            price.append(int(price_no_spp))
-            print(f'{art} = {price_no_spp}р')
-        except Exception as err:
-            price.append(None)
-            print(f'{art} = problem')
 
+            try:
+                price_wb = browser.find_element(By.CLASS_NAME, "price-block__final-price").text
+                price_wb = ''.join(price_wb.split(' ')[:-1])
+                price_with_spp.append(int(price_wb))
+            except Exception as err:
+                price_wb = None
+                price_with_spp.append(None)
+
+            try:
+                price_no_spp = browser.find_element(By.CLASS_NAME, "SppWidget_sppWidget__boldPrice__0J7RF").text
+                price_no_spp = ''.join(price_no_spp.split(' ')[:-1])
+                price.append(int(price_no_spp))
+            except Exception as err:
+                price_no_spp = None
+                price.append(None)
+
+            try:
+                sold_items = browser.find_element(By.CLASS_NAME, "ShortWidgetStat_shortWidgetStatColumn__nzQOE").text
+                sold_items = ''.join(sold_items.split(' ')[:-1])
+                sold.append(int(sold_items))
+            except Exception as err:
+                sold_items = None
+                sold.append(None)
+
+            try:
+                backs = browser.find_element(By.CLASS_NAME, "user-activity__count").text
+                fidbacks.append(int(backs))
+            except Exception as err:
+                backs = None
+                fidbacks.append(None)
+
+            print(f'{art} = {price_no_spp}р, {sold_items}, {backs}')
+
+
+for i in range(len(price_with_spp)):
+    sheet.cell(row=i+1, column=2, value=price_with_spp[i])
 
 for i in range(len(price)):
-    sheet.cell(row=i+1, column=2, value=price[i])
+    sheet.cell(row=i+1, column=3, value=price[i])
+
+for i in range(len(sold)):
+    sheet.cell(row=i+1, column=4, value=sold[i])
+
+for i in range(len(fidbacks)):
+    sheet.cell(row=i+1, column=5, value=fidbacks[i])
 
 workbook.save(filename='wb.xlsx')
-
-
---
+++ pass_captcha.py
@@ -0,0 +1,81 @@
+import time
+import pydub
+
+import requests
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import speech_recognition as sr
+
+name_audio_file = 'audio_captcha.mp3'
+login = 'pasechnik.pchelovod@mail.ru'
+password = 'Pchelovod17'
+
+
+def write_audio(url):
+    response = requests.get(url)
+    with open(f'{name_audio_file}', 'wb') as file:
+        file.write(response.content)
+
+
+def audio_to_text():
+    with open(f"{name_audio_file}", 'rb') as file:
+        path_to_mp3 = name_audio_file
+        path_to_wav = 'audio_file.wav'
+        sound = pydub.AudioSegment.from_file(path_to_mp3, 'mp3')
+        sound.export(path_to_wav, 'wav')
+        sample_audio = sr.AudioFile(path_to_wav)
+        r = sr.Recognizer()
+        with sample_audio as source:
+            audio = r.record(source)
+        key = r.recognize_google(audio)
+        return key
+
+
+def tru(browser):
+    time.sleep(3)
+    browser.find_element(By.XPATH, '/html/body/app-root/app-extension/div/app-extension-main/div/div/div/div[1]/div[3]/div/a[2]').click()
+    time.sleep(3)
+    # time.sleep(10)
+    # WebDriverWait(browser, 10).until(EC.frame_to_be_available_and_switch_to_it(
+    #     (By.CSS_SELECTOR, f"iframe[title='Виджет, содержащий вызов безопасности Cloudflare']")))
+    # time.sleep(10)
+    # WebDriverWait(browser, 10).until(
+    #     EC.element_to_be_clickable((By.CSS_SELECTOR, f"div[id='challenge-stage']"))).click()
+    # time.sleep(10)
+    # browser.implicitly_wait(10)
+    # time.sleep(10)
+    browser.find_element(By.ID, "email").send_keys(login)
+    time.sleep(3)
+    browser.find_element(By.XPATH, '/html/body/app-root/app-auth-layout/div[1]/div[2]/div/app-signin/form/div[2]/mg-input-password/mg-input-primitive/div/div[2]/input').send_keys(password)
+    time.sleep(3)
+    browser.find_element(By.CLASS_NAME, "w-100").click()
+    time.sleep(5)
+    # WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-reject-all-handler"))).click()
+    # WebDriverWait(browser, 10).until(
+    #     EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, f"iframe[title='reCAPTCHA']")))
+    #
+    # WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'recaptcha-anchor'))).click()
+    # time.sleep(10)
+    #
+    # browser.implicitly_wait(10)
+    #
+    # browser.switch_to.default_content()
+    WebDriverWait(browser, 10).until(EC.frame_to_be_available_and_switch_to_it(
+        (By.CSS_SELECTOR, f"iframe[title='текущую проверку reCAPTCHA можно пройти в течение ещё двух минут']")))
+
+    browser.find_element(By.ID, 'recaptcha-audio-button').click()
+    time.sleep(10)
+    src = browser.find_element(By.ID, "audio-source").get_attribute("src")
+    print(f"[INFO] Audio src: {src}")
+    write_audio(src)
+    key = audio_to_text()
+    print(key)
+
+    time.sleep(10)
+
+    browser.find_element(By.CSS_SELECTOR, 'input[id="audio-response"]').send_keys(key.lower())
+    time.sleep(10)
+    browser.find_element(By.ID, "audio-response").send_keys(Keys.ENTER)
+    time.sleep(10)
